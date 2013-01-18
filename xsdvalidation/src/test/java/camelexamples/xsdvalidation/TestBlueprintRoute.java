b69123c10fb9864aab05c36e38690b1c5df078dc
Jimmi Dyson <jimmi@feedthecoffers.org.uk>
2013-01-18 17:47:01 +0000

    Fix bad route & bad test - was always going to valid route as finally block to send to queue specified in destination header...

--- xsdvalidation/src/test/java/camelexamples/xsdvalidation/TestBlueprintRoute.java
+++ xsdvalidation/src/test/java/camelexamples/xsdvalidation/TestBlueprintRoute.java
@@ -1,64 +1,91 @@
 package camelexamples.xsdvalidation;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
-import org.apache.camel.Produce;
-import org.apache.camel.ProducerTemplate;
 import org.apache.camel.test.blueprint.CamelBlueprintTestSupport;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import javax.jms.*;
-import java.util.Properties;
 import java.util.Scanner;
 
 public class TestBlueprintRoute extends CamelBlueprintTestSupport {
 
-  @Produce(uri = "activemq:testqueue")
-  private ProducerTemplate template;
+  private static final String SOURCE_ENDPOINT = "activemq:testqueue";
+  private static final String VALID_QUEUE = "valid";
+  private static final String INVALID_ENDPOINT = "mock:invalid";
+  private static final String DESTINATION_HEADER = "destination";
+
+  private ActiveMQConnectionFactory connectionFactory;
+  private Connection connection;
+  private Session session;
+  private MessageConsumer consumer;
 
   @Override
   protected String getBlueprintDescriptor() {
     return "OSGI-INF/blueprint/camelContext.xml";
   }
 
-  private void consumeMessage(String destinationQueue, String expectedBody) throws Exception {
-    ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("vm://localhost?broker.persistent=false");
-    Connection connection = connectionFactory.createConnection();
+  @Before
+  public void setUpConnection() throws Exception {
+    connectionFactory = new ActiveMQConnectionFactory("vm://localhost?broker.persistent=false");
+    connection = connectionFactory.createConnection();
     connection.start();
-    Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-    MessageConsumer consumer = session.createConsumer(session.createQueue(destinationQueue));
-    Message message = consumer.receive(1000);
-    assertNotNull("Should have received a message from destination: " + destinationQueue, message);
+    session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+  }
 
-    TextMessage textMessage = assertIsInstanceOf(TextMessage.class, message);
-    assertEquals("Message body", expectedBody, textMessage.getText());
+  @After
+  public void tearDownConnection() throws Exception {
+    session.close();
+    connection.close();
+    connection.close();
+  }
+
+  private Message readMessageFromQueue(String destination) throws Exception {
+    MessageConsumer consumer = session.createConsumer(session.createQueue(VALID_QUEUE));
+    try {
+      Message message = consumer.receive(1000);
+      return message;
+    } finally {
+      consumer.close();
+    }
   }
 
   @Test
   public void testRouteWithInvalidMessage() throws Exception {
     // set mock expectations
-    getMockEndpoint("mock:invalid").expectedMessageCount(1);
+    getMockEndpoint(INVALID_ENDPOINT).expectedMessageCount(1);
 
     // send a message
     String messageToSend = new Scanner(getClass().getResourceAsStream("/invalid-shiporder.xml")).useDelimiter("\\Z").next();
-    template.sendBody(messageToSend);
+    template.sendBodyAndHeader(SOURCE_ENDPOINT, messageToSend, DESTINATION_HEADER, VALID_QUEUE);
 
     // assert mocks
     assertMockEndpointsSatisfied();
+
+    Message message = readMessageFromQueue(VALID_QUEUE);
+
+    assertNull("Should not have received a message from destination: " + VALID_QUEUE, message);
   }
 
   @Test
   public void testRouteWithValidMessage() throws Exception {
     // set mock expectations
-    getMockEndpoint("mock:invalid").expectedMessageCount(0);
+    getMockEndpoint(INVALID_ENDPOINT).expectedMessageCount(0);
 
     // send a message
     String messageToSend = new Scanner(getClass().getResourceAsStream("/valid-shiporder.xml")).useDelimiter("\\Z").next();
-    template.sendBodyAndHeader("activemq:testqueue", messageToSend, "DESTINATION", "valid");
+    template.sendBodyAndHeader(SOURCE_ENDPOINT, messageToSend, DESTINATION_HEADER, VALID_QUEUE);
 
     // assert mocks
     assertMockEndpointsSatisfied();
 
-    consumeMessage("valid", messageToSend);
+    Message message = readMessageFromQueue(VALID_QUEUE);
+
+    assertNotNull("Should have received a message from destination: " + VALID_QUEUE, message);
+
+    TextMessage textMessage = assertIsInstanceOf(TextMessage.class, message);
+    assertEquals("Message body", messageToSend, textMessage.getText());
   }
 
 }
